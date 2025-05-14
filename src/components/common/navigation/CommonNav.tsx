574af8fd35276d1bc24e742a5c98e8df8d0ffbaf
HeeJoo <k.lbtkr@gmail.com>
2025-05-14 15:39:17 +0900

    json파일 추가, 자동 링크 반영영

--- src/components/common/navigation/CommonNav.tsx
+++ src/components/common/navigation/CommonNav.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import styles from './CommonNav.module.scss';
+import { Link } from 'react-router-dom';
+import navJson from './nav.json';
 
 interface Navigation {
     index: number,
@@ -10,99 +12,14 @@ interface Navigation {
 }
 
 function CommonNav() {
-    const [navigation, setNavigation] = useState<Navigation[]>([
-        {
-            index:0,
-            path: '/edit',
-            label: "보도/편집 전용",
-            searchValue: 'edit',
-            isActive: false,
-        },
-        {
-            index:1,
-            path: '/following',
-            label: "팔로잉",
-            searchValue: 'following',
-            isActive: false,
-        },
-        {
-            index:2,
-            path: '/photoPlus',
-            label: "Unsplash Photo",
-            searchValue: 'photo',
-            isActive: false,
-        },
-        {
-            index:3,
-            path: '/oneColor',
-            label: "단색",
-            searchValue: 'one color',
-            isActive: false,
-        },
-        {
-            index:4,
-            path: '/3dRender',
-            label: "3D 랜더링",
-            searchValue: '3d rendering',
-            isActive: false,
-        },
-        {
-            index:5,
-            path: '/nature',
-            label: "자연",
-            searchValue: 'nature',
-            isActive: false,
-        },
-        {
-            index:6,
-            path: '/texture',
-            label: "텍스처 및 패턴",
-            searchValue: 'texture',
-            isActive: false,
-        },
-        {
-            index:7,
-            path: '/interior',
-            label: "인테리어",
-            searchValue: 'interior',
-            isActive: false,
-        },
-        {
-            index:8,
-            path: '/film',
-            label: "필름",
-            searchValue: 'film',
-            isActive: false,
-        },
-        {
-            index:9,
-            path: '/experimental',
-            label: "실험적인",
-            searchValue: 'experimental',
-            isActive: false,
-        },
-        {
-            index:10,
-            path: '/travel',
-            label: "여행",
-            searchValue: 'travel',
-            isActive: false,
-        },
-        {
-            index:11,
-            path: '/sports',
-            label: "스포츠",
-            searchValue: 'sports',
-            isActive: false,
-        },
-    ]);
+    const [navigation, setNavigation] = useState<Navigation[]>(navJson);
 
     // useState로 선언한 반응성을 가진 데이터를 기반으로 UI를 반복호출해보도록 한다.
     const navLinks = navigation.map((item : Navigation) => {
         return (
-            <div className={styles.navigation__menu} key={item.path}>
+            <Link to={item.path} className={styles.navigation__menu} key={item.path}>
                 <span className={styles.navigation__menu__label}>{item.label}</span>
-            </div>
+            </Link>
         )
     })
 
