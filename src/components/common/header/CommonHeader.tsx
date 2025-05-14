7c17b4869706cc8ba6f8cb9f20361c7fdf1b2429
HeeJoo <k.lbtkr@gmail.com>
2025-05-14 16:28:53 +0900

    시멘틱 태그 사용용

--- src/components/common/header/CommonHeader.tsx
+++ src/components/common/header/CommonHeader.tsx
@@ -2,7 +2,7 @@ import styles from './CommonHeader.module.scss'
 
 function CommonHeader() {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
         <div className={styles.header__logoBox}>
             <img src="src\assets\images\image-logo.jpg" alt="" className={styles.header__logoBox}/>
             <span className={styles.header__logoBox__title}>PhotoSplash</span>
@@ -12,7 +12,7 @@ function CommonHeader() {
             <button className={styles.header__profileBox__button}>북마크</button>
             <span className={styles.header__profileBox__userName}>Heejoo | link</span>
         </div>
-    </div>
+    </header>
   )
 }
 
